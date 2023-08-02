ae4922c217544a1c9078a00ea0b5b7a6b91bccf3
Marko Pojer <marko.pojer@outlook.com>
2023-08-02 14:41:02 +0200

    desktop newsletter

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './image.css'
-import mobileImage from '../../assets/illustration-sign-up-mobile.svg'
+import React from "react";
+import "./image.css";
+import mobileImage from "../../assets/illustration-sign-up-mobile.svg";
+import desktopImage from "../../assets/illustration-sign-up-desktop.svg";
 
 export const Image = () => {
   return (
-        <img src={mobileImage} className='header-image' alt="" />
-  )
-}
+    <div className="picture-wrapper">
+      <picture>
+        <source
+          srcset={desktopImage}
+          media="(min-width: 600px)"
+          className="desktop-image"
+        />
+        <img src={mobileImage} alt="" className="mobile-image" />
+      </picture>
+    </div>
+  );
+};
--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import mobileImage from '../../assets/illustration-sign-up-mobile.svg'
-import { Form } from '../Form/Form'
-import { Button } from '../Button/Button'
-import { Title } from '../Title/Title'
-import { Subtitle } from '../Subtitle/Subtitle'
-import { List } from '../List/List'
-import { Image } from '../Image/Image'
-
+import React from "react";
+import './newsletter.css'
+import { Form } from "../Form/Form";
+import { Button } from "../Button/Button";
+import { Title } from "../Title/Title";
+import { Subtitle } from "../Subtitle/Subtitle";
+import { List } from "../List/List";
+import { Image } from "../Image/Image";
 
 export const Newsletter = () => {
   return (
-    <>
-      <header>
+    <div className="container">
+      <div className="card-image">
         <Image />
-      </header>
-      <section>
+      </div>
+      <div className="card-body">
         <Title />
         <Subtitle />
         <List />
         <Form />
         <Button />
-      </section>
-    </>
-  )
-}
+      </div>
+    </div>
+  );
+};
